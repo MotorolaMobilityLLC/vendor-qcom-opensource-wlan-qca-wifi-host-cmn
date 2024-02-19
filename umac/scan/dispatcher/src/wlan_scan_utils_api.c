696a8f4358959c42c446fa2031c163773c1a29bd
Vinod Kumar Myadam <quic_vmyadam@quicinc.com>
2024-02-19 14:29:13 +0530

    qcacmn: Add check to avoid NULL pointer deference in parse MBSSID
    In malformed beacon frame may deference the NULL pointer while
    parsing MBSSID IE in util_scan_parse_mbssid will lead to crash.
    
    Add check in util_scan_parse_mbsssid for split_prof_start before
    passing to util_gen_new_ie and assign zero to split_prof_len
    whenever split_prof_start freed to avoid unanticipated scenario.
    
    Mot-CRs-fixed: (CR)
    CVE- fixed: CVE-2024-33010
    
    Change-Id: Ibb9739d6b5d1775ab52d59f9aa5050ca693cd926
    CRs-Fixed: 3717571
    Signed-off-by: Dharshan R <dharshan@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3017044
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Submit-Approved: Jira Key

--- umac/scan/dispatcher/src/wlan_scan_utils_api.c
+++ umac/scan/dispatcher/src/wlan_scan_utils_api.c
@@ -3595,6 +3595,8 @@ static QDF_STATUS util_scan_parse_mbssid(struct wlan_objmgr_pdev *pdev,
 			}
 
 			if (mbssid_info.split_prof_continue) {
+				if (!split_prof_start)
+					break;
 				nontx_profile = split_prof_start;
 				subie_len = split_prof_len;
 			} else {
@@ -3613,6 +3615,7 @@ static QDF_STATUS util_scan_parse_mbssid(struct wlan_objmgr_pdev *pdev,
 					qdf_mem_free(split_prof_start);
 					split_prof_start = NULL;
 					split_prof_end = NULL;
+					split_prof_len = 0;
 				}
 				continue;
 			}
@@ -3683,6 +3686,7 @@ static QDF_STATUS util_scan_parse_mbssid(struct wlan_objmgr_pdev *pdev,
 					qdf_mem_free(split_prof_start);
 					split_prof_start = NULL;
 					split_prof_end = NULL;
+					split_prof_len = 0;
 					qdf_mem_zero(&mbssid_info,
 						     sizeof(mbssid_info));
 				}
@@ -3697,6 +3701,7 @@ static QDF_STATUS util_scan_parse_mbssid(struct wlan_objmgr_pdev *pdev,
 				qdf_mem_free(split_prof_start);
 				split_prof_start = NULL;
 				split_prof_end = NULL;
+				split_prof_len = 0;
 			}
 			qdf_mem_free(new_frame);
 		}
