e491f2cd0d7f66d2884910b3592f24c06114aa24
Kiran Kumar Lokere <quic_klokere@quicinc.com>
2024-11-14 13:08:11 -0800

    qcacmn: Check EHT OP IE length for accessing optional fields
    Check the EHT OP IE length before accessing the optional fields
    to fix the possible out of bound memory access while handling
    the beacon frames.
    
    CRs-Fixed: 3967235
    Change-Id: Ia17264fcd918e6bdbbb99398463880ba9947076f
    (cherry picked from commit 83907af0f96c677d7f26ef0f1195f7d2ad0fbf74)

--- umac/scan/dispatcher/src/wlan_scan_utils_api.c
+++ umac/scan/dispatcher/src/wlan_scan_utils_api.c
@@ -258,6 +258,11 @@ util_scan_get_phymode_11be(struct wlan_objmgr_pdev *pdev,
 
 	if (QDF_GET_BITS(eht_ops->ehtop_param,
 			 EHTOP_INFO_PRESENT_IDX, EHTOP_INFO_PRESENT_BITS)) {
+		if (eht_ops->elem_len <
+			(offsetof(struct wlan_ie_ehtops, ccfs1) + 1)) {
+			scm_err("Invalid EHT OP IE length with EHT OP info");
+			return phymode;
+		}
 		width = QDF_GET_BITS(eht_ops->control,
 				     EHTOP_INFO_CHAN_WIDTH_IDX,
 				     EHTOP_INFO_CHAN_WIDTH_BITS);
@@ -322,6 +327,10 @@ util_scan_get_phymode_11be(struct wlan_objmgr_pdev *pdev,
 	if (QDF_GET_BITS(eht_ops->ehtop_param,
 			 EHTOP_PARAM_DISABLED_SC_BITMAP_PRESENT_IDX,
 			 EHTOP_PARAM_DISABLED_SC_BITMAP_PRESENT_BITS)) {
+		if (eht_ops->elem_len < sizeof(struct wlan_ie_ehtops)) {
+			scm_err("Invalid EHT OP IE len with dis_sc_bitmap");
+			return phymode;
+		}
 		scan_params->channel.puncture_bitmap =
 			QDF_GET_BITS(eht_ops->disabled_sub_chan_bitmap[0],
 				     0, 8);
@@ -437,6 +446,11 @@ util_scan_is_out_of_band_leak_eht(struct wlan_objmgr_pdev *pdev,
 	if (!QDF_GET_BITS(eht_ops->ehtop_param,
 			  EHTOP_INFO_PRESENT_IDX, EHTOP_INFO_PRESENT_BITS))
 		return false;
+
+	if (eht_ops->elem_len < (offsetof(struct wlan_ie_ehtops, ccfs1) + 1)) {
+		scm_err("Invalid EHT OP IE length with EHT OP info present");
+		return false;
+	}
 
 	ch_width = QDF_GET_BITS(eht_ops->control,
 				EHTOP_INFO_CHAN_WIDTH_IDX,
