3f16702c251e8faea0726418a0909ca09065852c
Jianmin Zhu <quic_jianminz@quicinc.com>
2024-12-23 19:51:09 -0800

    qcacmn: Correct EHT OP IE length checking for accessing optional fields
    Check the EHT OP IE length before accessing optional fields to prevent
    potential out-of-bounds memory access when handling beacon frames. The
    struct wlan_ie_ehtops includes the IE header, but elem_len does not include
    the IE header length (2 bytes), so correct length checking is necessary.
    
    Mot-CRs-fixed: (CR)
    CVE-fixed: CVE-2025-27057
    
    Change-Id: Ifa952c1a0bcf6661a505162aa3111bac5a1448c1
    CRs-Fixed: 4009252
    Signed-off-by: Dharshan R <dharshan@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3427441
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Submit-Approved: Jira Key

--- umac/scan/dispatcher/src/wlan_scan_utils_api.c
+++ umac/scan/dispatcher/src/wlan_scan_utils_api.c
@@ -223,8 +223,9 @@ util_scan_get_phymode_11be(struct wlan_objmgr_pdev *pdev,
 	if (QDF_GET_BITS(eht_ops->ehtop_param,
 			 EHTOP_INFO_PRESENT_IDX, EHTOP_INFO_PRESENT_BITS)) {
 		if (eht_ops->elem_len <
-			(offsetof(struct wlan_ie_ehtops, ccfs1) + 1)) {
-			scm_err("Invalid EHT OP IE length with EHT OP info");
+			(offsetof(struct wlan_ie_ehtops, ccfs1) - 1)) {
+			scm_err("Invalid EHT OP IE length %d with EHT OP info",
+				eht_ops->elem_len);
 			return phymode;
 		}
 		width = QDF_GET_BITS(eht_ops->control,
@@ -291,8 +292,9 @@ util_scan_get_phymode_11be(struct wlan_objmgr_pdev *pdev,
 	if (QDF_GET_BITS(eht_ops->ehtop_param,
 			 EHTOP_PARAM_DISABLED_SC_BITMAP_PRESENT_IDX,
 			 EHTOP_PARAM_DISABLED_SC_BITMAP_PRESENT_BITS)) {
-		if (eht_ops->elem_len < sizeof(struct wlan_ie_ehtops)) {
-			scm_err("Invalid EHT OP IE len with dis_sc_bitmap");
+		if (eht_ops->elem_len < sizeof(struct wlan_ie_ehtops) - 2) {
+			scm_err("Invalid EHT OP IE len %d with dis_sc_bitmap",
+				eht_ops->elem_len);
 			return phymode;
 		}
 		scan_params->channel.puncture_bitmap =
@@ -411,8 +413,9 @@ util_scan_is_out_of_band_leak_eht(struct wlan_objmgr_pdev *pdev,
 			  EHTOP_INFO_PRESENT_IDX, EHTOP_INFO_PRESENT_BITS))
 		return false;
 
-	if (eht_ops->elem_len < (offsetof(struct wlan_ie_ehtops, ccfs1) + 1)) {
-		scm_err("Invalid EHT OP IE length with EHT OP info present");
+	if (eht_ops->elem_len < (offsetof(struct wlan_ie_ehtops, ccfs1) - 1)) {
+		scm_err("Invalid EHT OP IE length %d with EHT OP info present",
+			eht_ops->elem_len);
 		return false;
 	}
 
