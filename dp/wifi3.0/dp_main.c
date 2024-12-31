89411a85ad8716301ed40f5f2c166b4eb6382e8f
Jinwei Chen <quic_jinweic@quicinc.com>
2024-12-31 00:05:51 -0800

    qcacmn: skip peer unmap timeout assert if fw is down
    If FW is down or recovery is ongoing, it's expected that
    peer unmap msg will be missed, skip to trigger assert
    for this case.
    
    Moto-CRs-fixed:(CR)
    
    Change-Id: Id80cd266e72262741d944815ac878a4c27d6d7f7
    CRs-Fixed: 4013772
    Reviewed-on: https://gerrit.mot.com/3192855
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Qiuhuan Zhao <zhaoqh3@lenovo.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

--- dp/wifi3.0/dp_main.c
+++ dp/wifi3.0/dp_main.c
@@ -6802,6 +6802,9 @@ void dp_peer_unmap_track_update(struct dp_soc *soc,
 	if (peer->peer_id == HTT_INVALID_PEER)
 		return;
 
+	if (qdf_is_recovering() || qdf_is_fw_down())
+		return;
+
 	elem = qdf_mem_malloc(sizeof(*elem));
 	if (!elem) {
 		dp_peer_info("failed to allocate memory for unmap tracking");
@@ -6876,7 +6879,16 @@ static void dp_peer_unmap_track_timer(void *arg)
 		       "id %d, delete timestamp 0x%llx",
 		       peer, QDF_MAC_ADDR_REF(peer->mac_addr.raw),
 		       peer->peer_id, elem->track_start_time);
-		qdf_assert_always(0);
+
+		/* It's expected if FW is down */
+		if (qdf_is_recovering() || qdf_is_fw_down()) {
+			dp_err("bypass assert as FW is down");
+			qdf_mem_free(elem);
+			if (peer)
+				dp_peer_unref_delete(peer, DP_MOD_ID_MISC);
+		} else {
+			qdf_assert_always(0);
+		}
 	}
 
 	cur_ts = qdf_get_log_timestamp();
